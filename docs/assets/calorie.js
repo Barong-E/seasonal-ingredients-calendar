ed52ade67741d777dcb362f368cd8ddc58bd6193
BeomJungKwon <barong@BeomJungKwons-MacBook-Air.local>
2026-07-13 22:33:36 +0900

    [자동] 칼로리 정밀 측정을 위한 안내 가이드 UI 구현 및 AI 프롬프트 개선

--- docs/assets/calorie.js
+++ docs/assets/calorie.js
@@ -36,7 +36,7 @@ In order to be iterable, non-array objects must have a [Symbol.iterator]() metho
             <div class="corner corner-br"></div>
           </div>
         </div>
-        <p class="scanner-guide-text">음식을 화면 중앙에 맞추고<br>아래 버튼을 눌러주세요</p>
+        <p class="scanner-guide-text"><span class="guide-tip-badge">💡 Tip</span> 숟가락, 젓가락, 포크 등을 그릇 옆에 <strong>내려놓고</strong> 함께 찍어주세요.<br>AI가 크기를 비교하여 칼로리를 더 정확하게 분석합니다.</p>
       </div>
 
       <div class="scanner-bottom-area">
